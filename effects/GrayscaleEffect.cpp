713abffcdba198c306ef12b41e8789da26b3d8c0
Lucien Rivière <lucien.riviere@epitech.eu>
2026-01-31 16:10:56 +0400

    [FIX] Grayscale now is gray...

--- effects/GrayscaleEffect.cpp
+++ effects/GrayscaleEffect.cpp
@@ -7,14 +7,8 @@ void GrayscaleEffect::apply(const sf::Image& src, sf::Image& dst, float /*time*/
     for (unsigned y = 0; y < s.y; ++y) {
         for (unsigned x = 0; x < s.x; ++x) {
             sf::Color c = src.getPixel(x, y);
-            unsigned char gray = static_cast<unsigned char>(
-                (0.299f * c.r + 0.587f * c.g + 0.114f * c.b)
-            );
-            // Lerp between original and gray by intensity
-            sf::Uint8 r = static_cast<sf::Uint8>(c.r * (1 - intensity) + gray * intensity);
-            sf::Uint8 g = static_cast<sf::Uint8>(c.g * (1 - intensity) + gray * intensity);
-            sf::Uint8 b = static_cast<sf::Uint8>(c.b * (1 - intensity) + gray * intensity);
-            dst.setPixel(x, y, sf::Color(r, g, b, c.a));
+            sf::Uint8 avg = static_cast<sf::Uint8>((c.r + c.g + c.b) / 3);
+            dst.setPixel(x, y, sf::Color(avg, avg, avg, c.a));
         }
     }
 }
